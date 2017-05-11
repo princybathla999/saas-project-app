1f688aa3f51b8e44f0b3a1aa3c2baf009a4697bc
Alpha Blog <princybathla999@yahoo.com>
2017-05-11 14:10:21 -0400

    users and tenant added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511154837) do
+ActiveRecord::Schema.define(version: 20170511161224) do
 
   create_table "members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "tenant_id"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170511154837) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "plan"
     t.index ["name"], name: "index_tenants_on_name", using: :btree
     t.index ["tenant_id"], name: "index_tenants_on_tenant_id", using: :btree
   end
